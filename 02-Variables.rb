e0e8b4448d43190dd922fa7c41e9441181579e59
RICALDI CASTRO Franklin Antolin <fricaldic@gmail.com>
2022-04-30 14:04:33 -0500

    Mi primer Commit

---
+++ 02-Variables.rb
@@ -0,0 +1,10 @@
+# variables
+variable = "Franklin"
+puts variable
+
+variable = 20
+puts variable
+
+#snake -> Vivora
+nombre_del_tutor = "Uriel"
+puts nombre_del_tutor
---
+++ 03-Numeros.rb
@@ -0,0 +1,26 @@
+#Flotantes -> 2.5
+#Enteros -> 10
+
+#Pasar un número entero a flotante
+numero1 = 10.to_f
+puts numero1
+
+#Pasar un número flotante a entero
+numero2 = 12.2321.to_i
+puts numero2
+
+#El número negativo lo combierte a positivo
+numero3 = -10.abs
+puts numero3
+
+#Te dice si un número es par
+numero4 = 3.even?
+puts numero4
+
+#Te dice si un número es par
+numero5 = 2.even?
+puts numero5
+
+#El que sigue despues de este número
+numero6 = 7.next
+puts numero6
